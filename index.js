187103c11b1867097ede4a8061c416c7cbc98fed
garcajuDeveloper <garciaabadj@gmail.com>
2019-05-10 01:07:16 +0200

    2 - Backend Server and test route created

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const mongoConnectionURI = 'mongodb://localhost:27017/forumDB'
+const app = require('./App');
+const backendPort = process.env.PORT || 3999;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoConnectionURI, { useNewUrlParser : true})
         .then(() => {
             console.log("DB online!");
+            app.listen(backendPort, () =>{
+                console.log("The Backend Server is listen in port: " + backendPort);
+            });
         })
         .catch(error => console.log(error));
